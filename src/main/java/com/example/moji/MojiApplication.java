255cf269404632efb2003c71b4e1ff4e689027b6
マヨ <mayo@naokinoiMac.local>
2020-06-19 14:37:24 +0900

    first commit

---
+++ src/main/java/com/example/moji/MojiApplication.java
@@ -0,0 +1,13 @@
+package com.example.moji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moji/MojiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MojiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
